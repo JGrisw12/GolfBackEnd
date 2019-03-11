96eea61e0f637c29cc5fd39675b8e339d8f07154
JGrisw12 <john.p.griswold@gmail.com>
2019-03-10 20:58:55 -0400

    Updatedrecords

---
+++ src/main/java/com/golfapp/GolfAppApplication.java
@@ -0,0 +1,12 @@
+package com.golfapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfAppApplication.class, args);
+	}
+}
